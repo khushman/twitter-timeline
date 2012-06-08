67d6cbca5a1def488e5a422c727d09541f372b5d
Prashish Rajbhandari <prashishh@gmail.com>
2012-06-08 17:39:58 +0530

    Updated index and css

--- index.php
+++ index.php
@@ -11,41 +11,36 @@
 
 <div class = "maincontainer borders">
 
-	<div class = "topcontainer borders"></div> 
+	<div class = "topcontainer"></div> 
 
-	<div class = "timelinecontainer borders">
+	<div class = "timelinecontainer">
 		
 		<div class = "timeline"></div>
 
 	<ol class = "timelineblock">
 
 	  <li> 
-   	    <span id="edge"></span>
-	    <div>Yesterday</div>
+   	    <span id="edge"></span><D-r>
 	    <p>just had a lunch ...</p>
 	  </li>
 
 	  <li> 
    	    <span id = "edge"></span>
-	    <div>Yesterday</div>
 	    <p>just had a lunch ...</p>
 	  </li>
 
 	  <li> 
    	    <span id="edge"></span>
-	    <div>Yesterday</div>
 	    <p>just had a lunch ...</p>
 	  </li>
 
 	  <li> 
    	    <span id="edge"></span>
-	    <div>Yesterday</div>
 	    <p>just had a lunch ...</p>
 	  </li>
 
 	  <li> 
    	    <span id="edge"></span>
-	    <div>Yesterday</div>
 	    <p>just had a lunch ...</p>
 	  </li>
 	 
@@ -55,9 +50,9 @@
 
 <script>
 $(function(){
-	$(".timelineblock li:odd").css("margin", "0px 0 20px 65px");
-	$(".timelineblock li:even").css("margin", "0px 0 20px 65px");
-	$(".timelineblock li:odd").css("float", "right").css("margin-right", "60px").css("clear", "both");
+	$(".timelineblock li:odd").css("margin", "0px 0 20px 25px");
+	$(".timelineblock li:even").css("margin", "0px 0 20px 25px");
+	$(".timelineblock li:odd").css("float", "right").css("margin-right", "20px").css("clear", "both");
 
 	$(".timelineblock li:odd").children("span#edge").addClass("ledge");
 	$(".timelineblock li:even").children("span#edge").addClass("redge");

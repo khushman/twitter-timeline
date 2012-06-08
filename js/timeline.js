143d263722a3e2e19213c07c91558ecfbb3c11de
Prashish Rajbhandari <prashishh@gmail.com>
2012-06-08 12:51:34 +0530

    Added css, images, js

---
+++ js/timeline.js
@@ -0,0 +1,5 @@
+$(".timelineblock li:odd").css("margin", "-10px 0 20px 90px");
+
+$(".timelineblock li:even").css("margin", "-10px 0 20px 90px");
+
+$(".timelineblock li:odd").css("float", "right").css("margin-right", "85px").css("clear", "both");
